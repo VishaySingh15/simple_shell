ef5713b58ff851b6d22ec061582efdc92d4bfc4a
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-25 14:43:05 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -33,7 +33,7 @@ void execute(cmd *cmd_struct, char *new_path)
 			{
 				write(STDIN_FILENO, cmd_struct->home, home_size);
 				write(STDIN_FILENO, err, err_size);
-				/*perror(cmd_struct->argv[0]);*/
+				perror(cmd_struct->argv[0]);
 				exit(1);
 			}
 		}
@@ -48,6 +48,6 @@ void execute(cmd *cmd_struct, char *new_path)
 	{
 		write(STDIN_FILENO, cmd_struct->home, home_size);
 		write(STDIN_FILENO, err, err_size);
-		/*perror(cmd_struct->argv[0]);*/
+		perror(cmd_struct->argv[0]);
 	}
 }
