0b9bee6f57d138784c6673e736d01c0a670fe870
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-25 14:21:29 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -13,13 +13,18 @@
 void execute(cmd *cmd_struct, char *new_path)
 {
 	pid_t child_pid;
+	char *err = ": 1: ";
+	size_t err_size = sizeof(err), home_size;
 	/*char *file_path = cmd_struct->argv[0];*/
 
 	if (check_file(new_path))
 	{
 		while ((child_pid = fork()) < 0)
 		{
-			perror(cmd_struct->home);
+			home_size = sizeof(cmd_struct->home);
+			write(STDIN_FILENO, cmd_struct->home, home_size);
+			write(STDIN_FILENO, err, err_size);
+			perror(cmd_struct->argv[0]);
 			exit(1);
 		}
 		/* In child process therefore, execute command */
@@ -28,6 +33,7 @@ void execute(cmd *cmd_struct, char *new_path)
 			/* Check if command is recognized by system */
 			if (execve(new_path, cmd_struct->argv, cmd_struct->env) < 0)
 			{
+				write(
 				perror("Unrecognized");
 				exit(1);
 			}
