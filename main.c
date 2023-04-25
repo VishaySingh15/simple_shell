6f9320ab553e6582bc31d871131c9d777da101ca
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-25 11:58:29 +0200

    update free command

--- main.c
+++ main.c
@@ -12,16 +12,18 @@
 int main(void)
 {
 	int nread;
-	char *command = malloc(100);
+	char *command;
 	const char *prompt = "myshell> ";
 	size_t nbytes = sizeof(prompt), line_size = 1024;
 
 	while (1)
 	{
+		command = malloc(100)
 		write(STDIN_FILENO, prompt, nbytes);
 		nread = getline(&command, &line_size, stdin);
 		if (nread == -1)
 		{
+			free(command);
 			return (0);
 		}
 		else if (nread > 1)
