b40c61a8aca23cff4fc1f9c5cfd5313ea5d7127e
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-26 18:24:13 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -34,6 +34,6 @@ void execute(cmd *cmd_struct, char *newpath)
 	else if (execve(newpath, cmd_struct->argv, cmd_struct->env) < 0)
 	{
 		perror(cmd_struct->argv[0]);
-		cmd_struct->exit_status = 2;
+		exit(2);
 	}
 }
