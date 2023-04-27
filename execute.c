37042d7d84ec6f8d1573c4db5c38ff5d52576f66
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-27 12:14:15 +0200

    output standard error

--- execute.c
+++ execute.c
@@ -11,12 +11,6 @@ void execute(cmd *cmd_struct, char *newpath)
 {
 	pid_t child_pid;
 
-	char err[] = ": 1: ", err2[] = ": ", not_found[] = "not found\n";
-	size_t err_size = sizeof(err), home_size, err2_size = sizeof(err2), comm_size;
-	size_t f_size = sizeof(not_found);
-
-	home_size = get_len(cmd_struct->home);
-	comm_size = get_len(cmd_struct->argv[0]);
 	if (check_file(newpath))
 	{
 		while ((child_pid = fork()) < 0)
@@ -28,11 +22,7 @@ void execute(cmd *cmd_struct, char *newpath)
 		{
 			if (execve(newpath, cmd_struct->argv, cmd_struct->env) < 0)
 			{
-				write(STDERR_FILENO, cmd_struct->home, home_size);
-				write(STDERR_FILENO, err, err_size);
-				write(STDERR_FILENO, cmd_struct->argv[0], comm_size);
-				write(STDERR_FILENO, err2, err2_size);
-				write(STDERR_FILENO, not_found, f_size);
+				perror(cmd_struct->home);
 				exit(1);
 			}
 		}
@@ -43,10 +33,6 @@ void execute(cmd *cmd_struct, char *newpath)
 	}
 	else if (execve(newpath, cmd_struct->argv, cmd_struct->env) < 0)
 	{
-		write(STDERR_FILENO, cmd_struct->home, home_size);
-		write(STDERR_FILENO, err, err_size);
-		write(STDERR_FILENO, cmd_struct->argv[0], comm_size);
-		write(STDERR_FILENO, err2, err2_size);
-		write(STDERR_FILENO, not_found, f_size);
+		perror(cmd_struct->home);
 	}
 }
