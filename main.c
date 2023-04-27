e1cdb7906acfa04a5ad9a44f016f767b539bee76
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-27 12:36:55 +0200

    changed env to environ

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
  * Return: void.
  */
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
 	int nread;
 	char *command;
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 		{
 			command[nread - 1] = '\0';
 			if (command)
-				parse(command, environ, argv[0]);
+				parse(command, env, argv[0]);
 			free(command);
 		}
 	}
